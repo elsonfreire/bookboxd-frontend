8225a4b6be8ca801568f8b2861b274525f2a2287
elsonfreire <elsonfreirefilho@gmail.com>
2025-07-29 15:50:00 -0300

    Basic page for login and register forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import { Login } from "./pages/Login";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <p>Welcome to Bookboxd!</p>
-    </>
-  );
+  return <Login />;
 }
 
 export default App;
