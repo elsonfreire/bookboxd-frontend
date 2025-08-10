838160fb63a446cec7f30181d37106fe559ed382
elsonfreire <elsonfreirefilho@gmail.com>
2025-08-10 14:26:44 -0300

    Populated Header and Sidebar with navbar items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,46 @@ type HeaderProps = {
 };
 
 export const Header = ({ onMenuClick }: HeaderProps) => {
+  const menuItems: Record<string, string> = {
+    Home: "/",
+    Books: "/books",
+    Search: "/search",
+    Reviews: "/reviews",
+  };
+
   const handleSearchOpen = () => {
     console.log("Open search");
   };
 
   return (
     <>
-      <div className="flex bg-gray-900 p-6 w-full text-white font-bold text-xl gap-6">
-        <button className="cursor-pointer md:hidden" onClick={onMenuClick}>
-          <MenuIcon size={28} />
-        </button>
-        <div>Popular</div>
-        <button className="ml-auto cursor-pointer" onClick={handleSearchOpen}>
-          <SearchIcon size={28} />
-        </button>
+      <div className="bg-gray-900 w-full flex text-white font-bold md:justify-center">
+        <div className="flex gap-6 p-6 md:p-4 w-full md:w-2xl">
+          <button className="cursor-pointer md:hidden" onClick={onMenuClick}>
+            <MenuIcon size={28} />
+          </button>
+          <a href="/" className="text-2xl md:text-3xl">
+            Bookboxd
+          </a>
+          <ul className="hidden md:flex ml-auto gap-4">
+            {Object.keys(menuItems).map((key) => (
+              <li key={key} className="content-center">
+                <a
+                  href={menuItems[key]}
+                  className="uppercase opacity-80 hover:opacity-100"
+                >
+                  {key}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="ml-auto md:ml-0 cursor-pointer"
+            onClick={handleSearchOpen}
+          >
+            <SearchIcon size={28} />
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,34 @@
+import { MenuIcon } from "../assets/icons/MenuIcon";
+
 type SidebarProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 
 export const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
+  const menuItems: Record<string, string> = {
+    Home: "/home",
+    Books: "/books",
+    Search: "/search",
+    Reviews: "/reviews",
+  };
+
   return (
     <>
       <div
-        className={`fixed top-0 left-0 h-full shadow-lg bg-white p-32 z-30 ${
+        className={`fixed top-0 left-0 h-full shadow-lg bg-gray-800 text-white px-8 py-10 w-64 z-30 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-200`}
-      ></div>
+      >
+        <ul className="flex flex-col gap-4">
+          {Object.keys(menuItems).map((key) => (
+            <li key={key} className="flex text-2xl gap-6">
+              <MenuIcon />
+              <a href={menuItems[key]}>{key}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
       {isOpen && (
         <div
           className="fixed bg-black z-20 inset-0 opacity-10"
